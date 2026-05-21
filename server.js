0a81be2c7e4c32577c36cc63faa108fc1f6c1db0
jeffrizz26 <jeffrizz26@gmail.com>
2026-05-21 21:58:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -81,9 +81,12 @@ app.put('/api/transactions/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running locally on port ${PORT}`);
+  });
+}
 
 module.exports = app;
+
